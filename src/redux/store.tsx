f2374e97468b778ab0e74e25a243b1336304936a
homazd <homa.zohdi@hotmail.com>
2023-06-27 17:50:15 +0330

    feat: add a counter reducer for testing

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { counterSlice } from "./features/counterSlice"
 
+
+// using configureStore from the redux toolkit library to create a new instance of the Redux store
 export const store = configureStore({
-    reducer: {},
-    devTools: process.env.NODE_ENV !== "production",
+  reducer: counterSlice.reducer,
 
-})
+});
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
